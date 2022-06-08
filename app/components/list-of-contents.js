bff22a3a6e3b4ff7472343d2b1f21bd8575c16ab
Swetha P <swetha-13762@swetha-13762.csez.zohocorpin.com>
2022-06-08 15:56:21 +0530

    Changed component files

--- app/components/list-of-contents.js
+++ app/components/list-of-contents.js
@@ -9,4 +9,26 @@ export default class ListOfContentsComponent extends Component {
   toggleList() {
     this.toggleProperty('toshowlist');
   }
+  
+  @tracked contentLists = [
+    { route: 'pages.getting-started', content: 'Getting Started' },
+    { route: 'pages.tutorial', content: 'Tutorial' },
+    { route: 'pages.concept1', content: 'Concept 1' },
+    { route: 'pages.concept2', content: 'Concept 2' },
+    { route: 'pages.concept3', content: 'Concept 3' },
+    { route: 'pages.summary', content: 'Summary' },
+  ];
+
+  @tracked listContent;
+
+  @action
+  addListContent() {
+    this.listContent = document.getElementById('listField').value;
+    console.log(this.listContent);
+    this.contentLists.pushObject({
+      route: 'pages.tutorial',
+      content: this.listContent,
+    });
+    document.getElementById('listField').value = '';
+  }
 }
