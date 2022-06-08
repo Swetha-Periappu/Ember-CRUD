adc1a068b1fd50dfac113324e6fde3b2b0fe357d
Swetha P <swetha-13762@swetha-13762.csez.zohocorpin.com>
2022-06-08 14:28:09 +0530

    changed

--- app/controllers/pages/concept1.js
+++ app/controllers/pages/concept1.js
@@ -1,12 +1,6 @@
 import Controller from '@ember/controller';
-import { action } from '@ember/object';
 import { tracked } from '@glimmer/tracking';
 
 export default class PagesConcept1Controller extends Controller {
   @tracked isEditable = false;
-
-  @action 
-  save(value){
-    this.send('saveEdits', value);
-  }
 }
--- app/controllers/pages/summary.js
+++ app/controllers/pages/summary.js
@@ -1,12 +1,7 @@
 import Controller from '@ember/controller';
-import { action } from '@ember/object';
 import { tracked } from '@glimmer/tracking';
 
 export default class PagesSummaryController extends Controller {
   @tracked isEditable = false;
 
-  @action 
-  save(value){
-    this.send('saveEdits', value);
-  }
 }
