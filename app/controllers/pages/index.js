723ce775b77408bfd026cb3e0fe527d186fe2b70
Swetha P <swetha-13762@swetha-13762.csez.zohocorpin.com>
2022-06-16 17:05:32 +0530

    added dynamic files

--- app/controllers/pages/index.js
+++ app/controllers/pages/index.js
@@ -1,5 +1,3 @@
 import Controller from '@ember/controller';
 
-export default class PagesIndexController extends Controller {
-
-}
+export default class PagesIndexController extends Controller {}
--- app/routes/pages/page.js
+++ app/routes/pages/page.js
@@ -3,7 +3,6 @@ import { service } from '@ember/service';
 import { findBy } from '@ember/array';
 
 export default class PagesPageRoute extends Route {
-
   @service pageLists;
 
   model(params) {
